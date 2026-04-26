1272e9b452527feeec0cfb34f19428bc7b3119de
Johannes Palvanen <palvanenj@gmail.com>
2026-04-26 18:21:27 +0300

    init project

---
+++ MeOrg.Tests/OrganizeTests.cs
@@ -0,0 +1,16 @@
+﻿namespace MeOrg.Tests;
+
+public class Tests
+{
+    [Fact]
+    public void Organize_Media_To_Target_Directory()
+    {
+        throw new NotImplementedException();
+    }
+
+    [Fact]
+    public void Ensure_Early_Exit_On_Missing_Permissions()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ MeOrg/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.CommandLine;
+using System.CommandLine.Parsing;
+
+RootCommand rootCommand = new("MeOrg is a media organizer CLI tool.");
+
+Command organizeCommand = new("organize")
+{
+    Description = "Used to organize media from an unorganized source directory into target directory."
+};
+Option<DirectoryInfo> sourceDirOption = new("--source")
+{
+    Description = "Unorganized media source directory.",
+    Required = true,
+    DefaultValueFactory = result =>
+    {
+        if (result.Tokens.Count == 0)
+        {
+            result.AddError("Source directory path not specified");
+        }
+
+        string dirPath = result.Tokens.Single().Value;
+        if (!Directory.Exists(dirPath))
+        {
+            result.AddError("Source directory does not exist");
+        }
+
+        return new DirectoryInfo(dirPath);
+    }
+};
+organizeCommand.Options.Add(sourceDirOption);
+organizeCommand.SetAction((parseResult) => Organize(parseResult.GetValue(sourceDirOption)!));
+
+rootCommand.Subcommands.Add(organizeCommand);
+
+ParseResult parseResult = rootCommand.Parse(args);
+
+return parseResult.Invoke();
+
+static void Organize(DirectoryInfo sourceDir)
+{
+    Console.WriteLine($"Source dir is {sourceDir.Name}");
+}
